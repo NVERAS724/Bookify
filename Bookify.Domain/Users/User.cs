e50e59e0082ba17432e3f9f1e5ccc801a21700e1
Nelson Veras <nvera@applaudostudios.com>
2024-07-31 18:56:44 -0400

    refact: unused references removed

--- Bookify.Domain/Users/User.cs
+++ Bookify.Domain/Users/User.cs
@@ -1,10 +1,6 @@
 ﻿using Bookify.Domain.Abstractions;
 using Bookify.Domain.Users.Events;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Bookify.Domain.Users
 {
