8f2e471c5a534a811aaa7dd9604ddeeb3ccf442b
Nelson Veras <nvera@applaudostudios.com>
2024-07-30 20:04:45 -0400

    feat: Currency, Description, Money and Name value objects

---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Apartments
+{
+    public record Currency
+    {
+
+        internal static readonly Currency None = new(string.Empty);
+        public static readonly Currency Usd = new(nameof(Usd).ToUpper());
+        public static readonly Currency Eur = new(nameof(Eur).ToUpper());
+
+        private Currency(string code) => Code = code;
+        public string Code { get; init; }
+
+        public static Currency FromCode(string code)
+        {
+            return All.FirstOrDefault(c => c.Code == code) ??
+                throw new ApplicationException("The currency code is invalid");
+        }
+
+        public static readonly IReadOnlyCollection<Currency> All = new[]
+        { 
+            Usd, 
+            Eur
+        };
+    }
+}
