52d8be1ce0b298857507188a221d80213d514139
Nelson Veras <nvera@applaudostudios.com>
2024-08-01 17:29:50 -0400

    feat: booking events

--- Bookify.Domain/Bookings/Events/BookingConfirmedDomainEvent.cs
+++ Bookify.Domain/Bookings/Events/BookingConfirmedDomainEvent.cs
@@ -3,6 +3,5 @@ using Bookify.Domain.Abstractions;
 
 namespace Bookify.Domain.Bookings.Events
 {
-    public record BookingReservedDomainEvent(Guid BookingId) : IDomainEvent;
-
+    public record BookingConfirmedDomainEvent(Guid BookingId) : IDomainEvent;
 }
