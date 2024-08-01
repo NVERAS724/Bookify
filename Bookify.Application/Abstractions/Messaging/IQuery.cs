436638fd70af809be7afa0a4c2faa38e4a82b069
Nelson Veras <nvera@applaudostudios.com>
2024-08-01 18:20:20 -0400

    feat: Abstraction and Messaging folder, ICommand, ICommandHandler, IQuery and IQueryHandler

---
+++ Bookify.Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,11 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+
+namespace Bookify.Application.Abstractions.Messaging
+{
+    public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+    {
+
+    }
+}
