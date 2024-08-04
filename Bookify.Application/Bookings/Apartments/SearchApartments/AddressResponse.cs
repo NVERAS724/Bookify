a69927ff70f78fd536abde98bd77a3785024a08c
Nelson Veras <nvera@applaudostudios.com>
2024-08-04 19:03:56 -0400

    feat: Apartment feature with SearchApartments response models, query and query handler

---
+++ Bookify.Application/Bookings/Apartments/SearchApartments/AddressResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace Bookify.Application.Bookings.Apartments.SearchApartments
+{
+    public sealed class AddressResponse
+    {
+
+
+        public AddressResponse(
+            string country, 
+            string state, 
+            string zipCode, 
+            string city, 
+            string street)
+        {
+            Country = country;
+            State = state;
+            ZipCode = zipCode;
+            City = city;
+            Street = street;
+        }
+        public string Country { get; init; }
+        public string State { get; init; }
+        public string ZipCode { get; init; }
+        public string City { get; init; }
+        public string Street { get; init; }
+    }
+}
---
+++ Bookify.Application/Bookings/Apartments/SearchApartments/ApartmentResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Bookify.Application.Bookings.Apartments.SearchApartments
+{
+    public sealed class ApartmentResponse
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+
+        public ApartmentResponse(string name, string description, decimal price, string currency, AddressResponse address)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Currency = currency;
+            Address = address;
+        }
+
+        public string Description { get; init; }
+        public decimal Price { get; init; }
+        public string Currency { get; init; }
+        public AddressResponse Address { get; set; }
+    }
+}
