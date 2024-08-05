09f980dfb9c1d3339d359fbfd81f2fb1a6ef124e
Nelson Veras <nvera@applaudostudios.com>
2024-08-05 18:21:38 -0400

    feat: User config

---
+++ Bookify.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Bookify.Domain.Users;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Bookify.Infrastructure.Configurations
+{
+    internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("users");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.FirstName)
+                .HasMaxLength(200)
+                .HasConversion(x => x.Value, x => new FirstName(x));
+
+            builder.Property(x => x.Lastname)
+                .HasMaxLength(200)
+                .HasConversion(x => x.Value, x => new LastName(x));
+
+            builder.Property(x => x.Email)
+                .HasMaxLength(400)
+                .HasConversion(x => x.Value, x => new Domain.Users.Email(x));
+
+            builder.HasIndex(user => user.Email).IsUnique();
+
+        }
+    }
+}
