6936804bea2bcd234e422ec970dd23e0a8f4cbb0
Nelson Veras <nvera@applaudostudios.com>
2024-08-05 18:16:24 -0400

    feat: apartment config

---
+++ Bookify.Infrastructure/Configurations/ApartmentConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Bookify.Domain.Apartments;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Bookify.Domain.Shared;
+
+
+namespace Bookify.Infrastructure.Configurations
+{
+    internal sealed class ApartmentConfiguration : IEntityTypeConfiguration<Apartment>
+    {
+        public void Configure(EntityTypeBuilder<Apartment> builder)
+        {
+            builder.ToTable("apartments");
+
+            builder.HasKey(x => x.Id);
+
+            builder.OwnsOne(x => x.Address);
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(200)
+                .HasConversion(x => x.Value, x => new Name(x));
+
+            builder.Property(x => x.Description)
+              .HasMaxLength(2000)
+              .HasConversion(x => x.Value, x => new Description(x));
+
+            builder.OwnsOne(x => x.Price, priceBuilder =>
+            {
+                priceBuilder.Property(money => money.Currency)
+                    .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+            });
+
+            builder.OwnsOne(x => x.CleaningFee, priceBuilder =>
+            {
+                priceBuilder.Property(money => money.Currency)
+                    .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+            });
+
+        }
+    }
+}
