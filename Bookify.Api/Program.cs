60232ebc34c433a01f88cd584c2e34ee0819df9f
Nelson Veras <nvera@applaudostudios.com>
2024-08-06 17:36:44 -0400

    feat: EntityFramework tools, .containers folder set to .gitignore, ApplicationBuilderExtension

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -1,5 +1,6 @@
 using Bookify.Infrastructure;
 using Bookify.Application;
+using Bookify.Api.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -17,6 +18,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
