4e6d412f697f1ac3650ce1da57ac178e3e76742d
Nelson Veras <nvera@applaudostudios.com>
2024-08-06 19:06:27 -0400

    feat: migrations folder for versioning

---
+++ Bookify.Infrastructure/Migrations/20240806215437_Fix_Street_Field.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_Street_Field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "address_steet",
+                table: "apartments",
+                newName: "address_street");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "address_street",
+                table: "apartments",
+                newName: "address_steet");
+        }
+    }
+}
