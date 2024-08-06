782cc8e33377f173bea69ed11f0f28e328aed9e9
Nelson Veras <nvera@applaudostudios.com>
2024-08-06 19:48:13 -0400

    fix: api versioning

--- Bookify.Api/Controllers/Apartments/ApartmentsController.cs
+++ Bookify.Api/Controllers/Apartments/ApartmentsController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Bookify.Api.Controllers.Apartments
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v1/apartments")]
     public class ApartmentsController : ControllerBase
     {
 
