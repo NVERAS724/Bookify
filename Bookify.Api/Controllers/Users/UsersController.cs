4ba9436495339df7c78efb58b22dc950aaa84dc3
Nelson Veras <nvera@applaudostudios.com>
2024-08-07 07:10:18 -0400

    feat: users controller

---
+++ Bookify.Api/Controllers/Users/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers.Users
+{
+    [ApiController]
+    [Route("api/v1/users")]
+    public class UsersController : ControllerBase
+    {
+    }
+}
