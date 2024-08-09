d9bdce551794e694f77d93e67a8f47960338e215
Nelson Veras <nveras@applaudostudios.com>
2024-08-09 19:29:11 -0400

    feat: domain unit testing

---
+++ Bookify.Domain.UnitTests/Users/UserTests.cs
@@ -0,0 +1,32 @@
+﻿using Bookify.Domain.Users.Events;
+using Bookify.Domain.Users;
+using FluentAssertions;
+
+namespace Bookify.Domain.UnitTests.Users
+{
+    public class UserTests : BaseTest
+    {
+        [Fact]
+        public void Create_Should_Raise_UserCreatedDomainEvent()
+        {
+            //Act
+            var user = User.Create(new FirstName(UserData.FirstName), new LastName(UserData.LastName), UserData.Email);
+
+            //Assert
+            var userCreatedDomainEvent = AssertDomainEventWasPublished<UserCreatedDomainEvent>(user);
+
+            userCreatedDomainEvent.UserId.Should().Be(user.Id);
+        }
+
+
+        [Fact]
+        public void Create_Should_AddRegisteredRoleToUser()
+        {
+            //Act
+            var user = User.Create(new FirstName(UserData.FirstName), new LastName(UserData.LastName), UserData.Email);
+
+            //Assert
+            user.Roles.Should().Contain(Role.Registered);
+        }
+    }
+}
